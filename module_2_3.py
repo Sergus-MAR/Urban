2adacb406f14bbc6219061b186f259b972c575a3
Sergus-MAR <nazsergey932@gmail.com>
2024-07-07 01:13:24 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+rezault_list = []
+i = 0
+while i < len(my_list):
+    if my_list[i] < 0:
+        break
+    elif my_list[i] > 0:
+            rezault_list.append(my_list[i])
+            i += 1
+    else:
+        i +=1
+        continue
+print(rezault_list)
