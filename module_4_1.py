ab38cd48a1f1fbdca8e480922588d5ae038b8eb1
Sergus-MAR <nazsergey932@gmail.com>
2024-07-22 21:38:41 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as sm
+from true_math import divide as hm
+
+result1 = sm(69, 3)
+result2 = sm(3, 0)
+result3 = hm(49, 7)
+result4 = hm(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+def divide(a,b):
+    '''Функция деления на "0" по правилам высшей математики'''
+    if b != 0:
+        return a/b
+    else:
+        return inf
+
