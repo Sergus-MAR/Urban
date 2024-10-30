9ceec8b8602c8a35524745f19aa18a6894bfc837
Sergus-MAR <nazsergey932@gmail.com>
2024-10-31 01:14:44 +0300

    module_9_3.py

---
+++ module_9_3.py
@@ -0,0 +1,9 @@
+﻿first = ['Strings', 'Student', 'Computers', 'Test', 'Test_first']
+second = ['Строка', 'Урбан', 'Компьютер', 'Test', 'Test_second']
+
+first_result = ((len(first_str) - len(second_str)) for (first_str, second_str) in zip(first, second)
+                if len(first_str) != len(second_str))
+second_result = (len(first[position]) == len(second[position]) for position in range(min(len(first), len(second))))
+
+print(list(first_result))
+print(list(second_result))
