6822194b502c6c12a7d09462a3b458289fb5b7c5
Sergus-MAR <nazsergey932@gmail.com>
2024-11-04 02:24:52 +0300

    module_9_6.py

---
+++ module_9_6.py
@@ -0,0 +1,13 @@
+﻿def all_variants(text):
+    i = 0
+    symbols = len(text) 
+    while i != symbols:
+        j = 0
+        while j != (symbols - i):
+            yield text[j:i+j+1]
+            j += 1
+        i += 1
+
+a = all_variants("abc")
+for i in a:
+    print(i)
